a7b62c0d3e0567a0f92c715fbe22db588935aa8e
AartiPundalik <aartipundalik68@gmail.com>
2023-04-20 19:51:39 +0530

    [Aarti] Add : UC2 - Queue Example

---
+++ LinkedList_Programs/LinkedList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblems
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list ", node.Data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+            }
+            Console.WriteLine("Linked list is: ");
+            while (temp != null)
+            {
+                Console.WriteLine(temp.Data + " ");
+                temp = temp.Next;
+            }
+            Console.ReadLine();
+        }
+        public void AddReverse(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                node.Next = temp;
+                head = node;
+            }
+            Console.WriteLine($"{node.Data} is now Head");
+            Console.ReadLine();
+        }
+    }
+}
+    
+    
+
+       
+    
+
+
